30953531e20fa5b8aebc159561b3ecbb0ff03767
Ahsan Sakib <ahsansakib@Ahsans-MacBook-Pro.local>
2023-06-18 07:43:38 +0600

    [unit-test][photoapp] project creation

---
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Ahsan Sakib on 18/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
